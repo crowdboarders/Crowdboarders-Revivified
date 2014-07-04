85738dedb1a00888b8e354db616a13e2e57fafd5
MattAurich <mattaurich@aol.com>
2014-07-04 12:48:19 -0500

    update 07/04/14:responsive nav and districts page start

--- app/scripts/navbar.js
+++ app/scripts/navbar.js
@@ -1,4 +1,18 @@
-$(document).ready(function() {
-
-
-})
+$(document).ready(function(){
+  $(".navbar-responsive").click(function() {
+     if ($(".navbar-responsive > ul").css("display") == "none") {
+      $(".navbar-responsive > ul").css("display","block");
+     }
+     else {
+      $(".navbar-responsive > ul").css("display","none");
+     }
+  });
+   $("#last").click(function(){
+     if ($("#last > ul").css("display") == "none") {
+      $("#last > ul").css("display","block");
+     }
+     else {
+      $("#last > ul").css("display","none");
+     }
+  });
+});
